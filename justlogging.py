324b77b78a537d9ca40daf76a65da8f8841e8008
Robert Beekman <robertbeekman@noname.(none)>
2009-11-18 16:05:58 +0100

    added alert feature

--- justlogging.py
+++ justlogging.py
@@ -10,17 +10,20 @@ class Justlogging:
     self.api_key = api_key
     self.log_key = log_key
   
-  def log(self, entry, log_key=None):
+  def alert(self, entry, log_key=None):
+    self.log(entry, log_key, True)
+  
+  def log(self, entry, log_key=None, alert=False):
     if log_key == None:
       log_key = self.log_key
       
-    params = urllib.urlencode({'access_key': self.api_key, 'log_key': log_key, 'entry': entry})
+    params = urllib.urlencode({'access_key': self.api_key, 'log_key': log_key, 'entry': entry, 'alert':alert})
     req = Request("http://logs.justlogging.com/log", params)
     
     try:
       response = urlopen(req)
     except HTTPError, e:
-      if e.code == 201:
+      if e.code == 201: # Our API returns 201 on success urllib sees it as an error.
         return True
       else:
         return False
